cee4473bda4843b637631382f5b19ae6d27bc4de
29Hac07Cah96$ <hectorc2907@gmail.com>
2024-10-15 07:31:17 -0300

    configuramos el controlador para eliminar los posts

--- backend/src/controllers/postsController.js
+++ backend/src/controllers/postsController.js
@@ -99,7 +99,28 @@ export const updatePost = async (req, res) => {
 };
 
 // Eliminar un post
-export const deletePost = (req, res) => {
-  // Aún no implementado: debería eliminar un post específico basado en su ID
-  res.send("deleting a post");
+export const deletePost = async (req, res) => {
+  // Intenta ejecutar el bloque de código para eliminar un post
+  try {
+    // Desestructura el ID del post desde los parámetros de la solicitud
+    const { id } = req.params;
+
+    // Busca y elimina el post en la base de datos utilizando el ID proporcionado
+    const post = await Posts.findByIdAndDelete(id);
+
+    // Si el post no se encuentra, responde con un estado 404 (No Encontrado)
+    if (!post) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
+    }
+
+    // Responde con un estado 200 (OK) y un mensaje de éxito junto con el post eliminado
+    return res
+      .status(200)
+      .json({ success: true, message: "Post Deleted", post });
+  } catch (error) {
+    // Si ocurre un error, responde con un estado 500 (Error Interno del Servidor) y el mensaje de error
+    return res.status(500).json({ success: false, message: error.message });
+  }
 };
