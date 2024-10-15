3b8d5138185ea72f9dfcbaec800c788fb6b874f3
29Hac07Cah96$ <hectorc2907@gmail.com>
2024-10-15 06:17:43 -0300

    agregamos las rutas a app.js para su uso y probamos su funcionamiento correcto

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,33 @@
 // Importa el módulo express para crear aplicaciones web
 import express from "express";
-// Importa el middleware morgan para registrar las solicitudes HTTP
+
+// Importa el middleware morgan para registrar las solicitudes HTTP en la consola
 import morgan from "morgan";
+
+// Importa las rutas relacionadas con los posts desde el archivo correspondiente
+import postsRoutes from "./routes/postsRoute.js";
 
 // Crea una nueva instancia de una aplicación Express
 const app = express();
 
+// Configuración de middlewares:
+
 // Middleware para parsear el cuerpo de las solicitudes en formato JSON
 app.use(express.json());
-// Middleware para parsear el cuerpo de las solicitudes con datos URL-encoded
+
+// Middleware para parsear datos en formato URL-encoded (por ejemplo, formularios HTML)
 app.use(express.urlencoded({ extended: false }));
-// Middleware para registrar las solicitudes HTTP en la consola con formato 'dev'
+
+// Middleware para registrar las solicitudes HTTP en la consola en formato 'dev'
 app.use(morgan("dev"));
 
-// Definimos las rutas.
+// Definimos las rutas de la aplicación:
 
-// Ruta directa de prueba para verificar el estado del backend.
+// Ruta directa de prueba para verificar que el backend está funcionando correctamente
 app.get("/", (req, res) => res.send("test"));
 
-// Exporta la instancia de la aplicación Express para usarla en otros archivos
+// Usamos las rutas relacionadas con los posts, bajo el prefijo '/api'
+app.use("/api", postsRoutes);
+
+// Exporta la instancia de la aplicación Express para que pueda ser utilizada en otros archivos
 export { app };
