85dfb294baf961fe08ba775eecf6b6efe0e3c11b
29Hac07Cah96$ <hectorc2907@gmail.com>
2024-10-15 16:17:20 -0300

    configuramos las rutas junto con sus elementos correspondientes y direcciones, incluimos el Toaster en el frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import PostForm from "./pages/PostForm";
+import NotFoundPage from "./pages/NotFoundPage";
+import { Toaster } from "react-hot-toast";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="bg-neutral-900 min-h-screen flex items-center">
+      <div className="px-10 container m-auto py-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/new" element={<PostForm />} />
+          <Route path="/:id" element={<PostForm />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <Toaster />
+      </div>
+    </div>
+  );
 };
 
 export default App;
